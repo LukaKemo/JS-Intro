d2320ab734e71692abd7bfa3e98f5b081bce4dd2
LukaKemo <kemenovic.luka@gmail.com>
2021-04-08 21:27:00 +0200

    Task 1 - SOLVED

---
+++ script.js
@@ -0,0 +1,9 @@
+function printLines() {
+    console.log(secondLine);
+}
+
+var firstLine = "I'm your first line!";
+console.log(firstLine);
+
+var secondLine = "I'm your second line!";
+console.log(secondLine);
